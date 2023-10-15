4825cd45412ee3846873270488e62a57c42c0343
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-15 00:21:13 -0400

    Initial Commit

---
+++ Hexagon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hexagon
+//
+//  Created by Temitope Adebayo on 2023-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hexagon/HexagonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonApp.swift
+//  Hexagon
+//
+//  Created by Temitope Adebayo on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct HexagonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
